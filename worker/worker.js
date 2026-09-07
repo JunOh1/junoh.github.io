128f1d68dc0030b75c657e7a5eb0974de0926b0c
Jun <jun@Juns-MacBook-Pro.local>
2026-09-07 12:26:07 -0400

    Add bot IP filters

--- worker/worker.js
+++ worker/worker.js
@@ -484,7 +484,8 @@ function isBannedBotIp(ip) {
     "212.156.70.154",
     "31.171.155.142",
     "64.89.160.5",
-    "181.214.206.158"
+    "181.214.206.158",
+    "95.85.238.181"
   ].includes(ip);
 }
 
@@ -1366,7 +1367,8 @@ export default {
             '212.156.70.154',
             '31.171.155.142',
             '64.89.160.5',
-            '181.214.206.158'
+            '181.214.206.158',
+            '95.85.238.181'
           ) THEN 1
           WHEN lower(coalesce(org,'')) LIKE '%cloudflare%' THEN 1
           WHEN lower(coalesce(org,'')) LIKE '%amazon%' THEN 1
@@ -1829,7 +1831,8 @@ export default {
             '212.156.70.154',
             '31.171.155.142',
             '64.89.160.5',
-            '181.214.206.158'
+            '181.214.206.158',
+            '95.85.238.181'
           ) THEN 1
           WHEN lower(coalesce(org,'')) LIKE '%cloudflare%' THEN 1
           WHEN lower(coalesce(org,'')) LIKE '%collyer quay%' THEN 1
